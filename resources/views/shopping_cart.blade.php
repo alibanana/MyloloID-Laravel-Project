8972e48df23d92ce895e62e45e60acc659b1d6fc
ExtGmrJasonZ <43334472+ExtGmrJasonZ@users.noreply.github.com>
2020-05-05 20:08:15 +0700

    Update shopping_cart.blade.php

--- resources/views/shopping_cart.blade.php
+++ resources/views/shopping_cart.blade.php
@@ -60,8 +60,48 @@
   </table>
 </div>
 
+
 {{-- Table Shopping Cart Mobile Version --}}
 <div class="d-md-none">
-  <h1 class="text-black">Disini Nanti Jas</h1>
+  <h1 class="text-black">TEST - CART - MOBILE</h1>
+  <div class="card w-90">
+    <div class="card-body">
+      <div class = "product-thumbnail">
+        <img src="/images/cartpage/product1.jpg" alt="" class="rounded float-left" class="img-thumbnail" style="width:200px;height:200px;">
+      </div>
+
+      <h4 class="product-name" style= "color:black" style="text-align:right">Mylo Crop Sleeve Denim</h4>
+      <p class="product-price" style= "color:black" style="text-align:right">IDR 775K</p>
+      <p class="product-quantity">
+        <div class="input-group quantity-input text-align-center justify-content-center">
+          <div class="input-group-prepend">
+            <button type="button" class="btn btn-primary minus-button js-btn-minus">&minus;</button>
+          </div>
+          <input type="text" class="form-control text-center quantity-text" value="1" readonly>
+          <div class="input-group-append">
+            <button type="button" class="btn btn-primary plus-button js-btn-plus" >&plus;</button>
+          </div>    
+    </div>
+  </div>
+
+  <div class="card w-90">
+    <div class="card-body">
+    <div class = "product-thumbnail">
+        <img src="/images/cartpage/product2.jpg" alt="" class="rounded float-left" class="img-thumbnail" style="width:200px;height:200px;">
+    </div>
+
+    <h4 class="product-name" style= "color:black" style="text-align:right">Mylo Half Ruffles Shirtdress</h4>
+    <p class="product-price" style= "color:black" style="text-align:right">IDR 445K</p>
+    <p class="product-quantity">
+      <div class="input-group quantity-input text-align-center justify-content-center">
+        <div class="input-group-prepend">
+          <button type="button" class="btn btn-primary minus-button js-btn-minus">&minus;</button>
+        </div>
+        <input type="text" class="form-control text-center quantity-text" value="1" readonly>
+        <div class="input-group-append">
+          <button type="button" class="btn btn-primary plus-button js-btn-plus">&plus;</button>
+        </div>           
+    </div>
+  </div>
 </div>
 @endsection
